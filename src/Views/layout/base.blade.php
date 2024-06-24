f64b201198f2e5691d940a9e687d443d757875d3
Ade Yusuf <adeyusuf005@gamil.com>
2024-06-24 17:16:47 +0700

    Update

--- src/Views/layout/base.blade.php
+++ src/Views/layout/base.blade.php
@@ -1,14 +1,15 @@
 <!doctype html>
-<html>
-
+<html lang="en">
 <head>
+    <title>@yield('title')</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/0cc2808a01.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="relative bg-gray-100">
         @yield('content')
     </h1>
 </body>
